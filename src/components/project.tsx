e56d1fb1639e6e2bd8d524f32f99c89675326e6d
Jeff Claybrook <jeffclaybrook@Jeffs-MacBook-Pro.local>
2024-07-13 13:53:19 -0500

    Updated text color

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -15,7 +15,7 @@ const Project = ({ title, href, image }: ProjectProps) => {
    target="_blank"
    rel="noreferrer"
   >
-   <div className="hero-overlay flex flex-col justify-center items-center py-24 bg-opacity-65 text-slate-300 hover:bg-opacity-80">
+   <div className="hero-overlay flex flex-col justify-center items-center py-24 bg-opacity-65 text-slate-200 hover:bg-opacity-80">
     <h3 className="text-2xl tracking-wide mb-3.5">{title}</h3>
     <NewTab />
    </div>
--- src/components/testimonial.tsx
+++ src/components/testimonial.tsx
@@ -7,7 +7,7 @@ type TestimonialProps = {
 
 const Testimonial = ({ quote, name, title, image }: TestimonialProps) => {
  return (
-  <article className="hero max-w-6xl mx-auto rounded-3xl overflow-hidden text-slate-300" style={{ backgroundImage: `url(${image})`}}>
+  <article className="hero max-w-6xl mx-auto rounded-3xl overflow-hidden text-slate-200" style={{ backgroundImage: `url(${image})`}}>
    <div className="hero-overlay flex flex-col justify-center bg-opacity-70 p-6 min-h-[260px]">
     <h3 className="text-2xl tracking-wide mb-3.5">&quot;{quote}&quot;</h3>
     <h4 className="text-lg tracking-wide mb-1.5">{name}</h4>
