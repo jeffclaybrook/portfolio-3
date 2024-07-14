a89041d756dbd368c728477de0e2b110e1651d03
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-07-14 13:00:24 -0500

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Plus_Jakarta_Sans } from "next/font/google"
+import { GoogleAnalytics } from "@next/third-parties/google"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import "./globals.css"
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={plusJakartaSans.className}>
+        <GoogleAnalytics gaId="G-SJSXDE92X3" />
         <Navbar />
         {children}
         <Footer />
